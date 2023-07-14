a490c038535b455502e57ab2af4c1756ae3289eb
techni-ca <48330373+techni-ca@users.noreply.github.com>
2023-07-14 17:55:46 -0600

    Expand test suite

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,10 @@
 const mergeSort = require('./mergesort');
-
+const longArray = []
+for (let i = 0; i < 12500; i++) {
+  const randomNumber = Math.trunc(Math.random() * 10000,0)
+  longArray.push(randomNumber)
+}
+const sortedLongArray = [...longArray].sort((a, b) => (a-b))
 describe('mergeSort()', function() {
   test('handles empty set', function() {
     expect(mergeSort([])).toEqual([])
@@ -16,10 +21,16 @@ describe('mergeSort()', function() {
   test('handles duplicates', function() {
     expect(mergeSort([1, 2, 1, 2, 1])).toEqual([1, 1, 1, 2, 2])
   })
+  test('handles longer numbers', function() {
+    expect(mergeSort([1,10,3,30,222222])).toEqual([1,3,10,30,222222])
+  })
   test('handles characters', function() {
     expect(mergeSort('andrew')).toEqual(['a','d','e','n','r','w'])
   })
   test('handles strings', function() {
     expect(mergeSort(['one', 'two', 'three', 'four', 'five'])).toEqual(['five', 'four', 'one', 'three', 'two'])
+  })
+  test('12,500 element array (generated randomly)', function() {
+    expect(mergeSort(longArray)).toEqual(sortedLongArray)
   })
 });
