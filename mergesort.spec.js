928e45b59d52dc3df9ffb535d6534f9e713622af
techni-ca <48330373+techni-ca@users.noreply.github.com>
2023-07-15 06:54:40 -0600

    Merge from the right for speed

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,10 +1,12 @@
-const mergeSort = require('./mergesort');
+const mergeSort = require('./mergesort')
+const mergeSort2 = require('./mergesort2')
 const longArray = []
-for (let i = 0; i < 12500; i++) {
+for (let i = 0; i < 10000; i++) {
   const randomNumber = Math.trunc(Math.random() * 10000,0)
   longArray.push(randomNumber)
 }
 const sortedLongArray = [...longArray].sort((a, b) => (a-b))
+
 describe('mergeSort()', function() {
   test('handles empty set', function() {
     expect(mergeSort([])).toEqual([])
@@ -25,12 +27,43 @@ describe('mergeSort()', function() {
     expect(mergeSort([1,10,3,30,222222])).toEqual([1,3,10,30,222222])
   })
   test('handles characters', function() {
-    expect(mergeSort('andrew')).toEqual(['a','d','e','n','r','w'])
+    expect(mergeSort(['b','c','a'])).toEqual(['a','b','c'])
   })
   test('handles strings', function() {
     expect(mergeSort(['one', 'two', 'three', 'four', 'five'])).toEqual(['five', 'four', 'one', 'three', 'two'])
   })
-  test('12,500 element array (generated randomly)', function() {
+  test('10000 element array (generated randomly)', function() {
     expect(mergeSort(longArray)).toEqual(sortedLongArray)
   })
-});
+})
+describe('mergeSort2()', function() {
+  test('handles empty set', function() {
+    expect(mergeSort2([])).toEqual([])
+  })
+  test('handles ordered array', function() {
+    expect(mergeSort2([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5])
+  })
+  test('handles reverse ordered array', function() {
+    expect(mergeSort2([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5])
+  })
+  test('handles mixed up array', function() {
+    expect(mergeSort2([3, 2, 5, 1, 4])).toEqual([1, 2, 3, 4, 5])
+  })
+  test('handles duplicates', function() {
+    expect(mergeSort2([1, 2, 1, 2, 1])).toEqual([1, 1, 1, 2, 2])
+  })
+  test('handles longer numbers', function() {
+    expect(mergeSort2([1,10,3,30,222222])).toEqual([1,3,10,30,222222])
+  })
+  test('handles characters', function() {
+    expect(mergeSort2(['b','c','a'])).toEqual(['a','b','c'])
+  })
+  test('handles strings', function() {
+    expect(mergeSort2(['one', 'two', 'three', 'four', 'five'])).toEqual(['five', 'four', 'one', 'three', 'two'])
+  })
+  test('10000 element array (generated randomly)', function() {
+    expect(mergeSort2(longArray)).toEqual(sortedLongArray)
+  })
+})
+
+
---
+++ mergesort2.js
@@ -0,0 +1,22 @@
+function mergeSort2 (arrayIn) {
+  if (arrayIn.length < 2) return arrayIn
+  const halfway = Math.trunc(arrayIn.length / 2, 0)
+  const left = arrayIn.slice(0, halfway)
+  const right = arrayIn.slice(halfway)
+  return merge(mergeSort2(left), mergeSort2(right))
+}
+function merge (array1, array2) {
+  const a1l = array1.length - 1
+  const a2l = array2.length - 1
+  if (a1l < 0) return array2
+  if (a2l < 0) return array1
+  let last = 0
+  if (array1[a1l] < array2[a2l]) {
+    last = array2.pop()
+  } else {
+    last = array1.pop()
+  } 
+  return merge(array1, array2).concat([last])
+}
+
+module.exports = mergeSort2
