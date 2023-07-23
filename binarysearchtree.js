65347ac5e69e1b34d514eb4939183590afa08fda
techni-ca <48330373+techni-ca@users.noreply.github.com>
2023-07-23 10:26:10 -0600

    Implement Binary Search Tree

---
+++ binarysearchtree.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor (inValue, left, right) {
+    this.value = inValue
+    this.leftNode = left
+    this.rightNode = right
+  }
+}
+class Tree {
+  constructor (inArray) {
+    this.root = buildTree(inArray.sort())
+  }
+  buildTree (sortedArray) {
+    const halfway = trunc(sortedArray.length / 2,0)
+    const centreOfArray = sortedArray[halfway]
+    const leftOfArray = sortedArray.slice(0,halfway)
+    const rightOfArray = sortedArray.slice(halfway + 1)
+    const leftNode = (leftOfArray === []) ? null : this.buildTree(leftOfArray)
+    const rightNode = (rightOfArray === []) ? null : this.buildTree(rightOfArray)
+    return new Node(centreOfArray, leftNode, rightNode)
+  }
+}
+
+module.exports = Tree
---
+++ binarysearchtree.spec.js
@@ -0,0 +1,1 @@
+const tree = require('./binarysearchtree')
